258ebc1bc688696e495a0a2da97e75c872bbc1d4
Peter Weyand <pweyand@gmail.com>
2017-08-08 10:53:46 -0500

    status 400 error: bad request

--- scalarest/app/controllers/Application.scala
+++ scalarest/app/controllers/Application.scala
@@ -23,28 +23,20 @@ class Application @Inject()(cc: ControllerComponents) extends AbstractController
     (JsPath \ "testDOUBLE").read[Double]
   )(testPOST.apply _)
 
-  implicit class RichResult (result: Result) {
-    def enableCors =  result.withHeaders(
-      "Access-Control-Allow-Origin" -> "*"
-      , "Access-Control-Allow-Methods" -> "OPTIONS, GET, POST, PUT, DELETE, HEAD"   // OPTIONS for pre-flight
-      , "Access-Control-Allow-Headers" -> "Accept, Content-Type, Origin, X-Json, X-Prototype-Version, X-Requested-With" //, "X-My-NonStd-Option"
-      , "Access-Control-Allow-Credentials" -> "true"
-    )
-  }
-
   val testSTRINGread = (JsPath \ "testSTRING").read[String]
 
   val testDOUBLEread = (JsPath \ "testDOUBLE").read[Double]
+
 
   def saveTest = Action(BodyParsers.parse.json) { request =>
     val testResult = request.body.validate[testPOST]
     testResult.fold(
       errors => {
-        BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors))).enableCors
+        BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors)))
       },
       testpost => {
         testPOST.save(testpost)
-        Ok(Json.obj("status" ->"OK", "message" -> ("TestGET "+testpost+" saved."))).enableCors
+        Ok(Json.obj("status" ->"OK", "message" -> ("TestGET "+testpost+" saved.")))
       }
     )
   }
